02569401cd1647ac636ed174e636b643ee3c2453
nianhuawong <nianhuawong@126.com>
2025-03-29 00:16:23 +0800

    refactor: 重构参数读取逻辑以支持从主配置文件加载
    重构了 `Parameters` 类的初始化逻辑，使其支持从主配置文件 `main.json` 加载案例配置文件路径，并增加了必要的字段校验。此改动提高了代码的可维护性和灵活性，避免了硬编码配置路径的问题。

--- data_structure/parameters.py
+++ data_structure/parameters.py
@@ -1,11 +1,20 @@
 import json
+from pathlib import Path
 
 
 class Parameters:
-    def __init__(self, config_file):
+    def __init__(self, get_param_from, json_file=None):
+        # 配置文件来源，FROM_MAIN_JSON 或 FROM_CASE_JSON
+        self.get_param_from = get_param_from
+        self.config_file = []
+
+        if json_file:
+            json_file = Path(json_file).resolve()
+        self.json_file = json_file
+
+        # 算例参数
         self.debug_level = 0  # 调试级别，0-不输出，1-输出基本信息，2-输出详细信息
         self.part_params = []  # 网格生成部件参数
-        self.config_file = config_file
         self.input_file = []
         self.output_file = []
 
@@ -13,15 +22,28 @@ class Parameters:
 
     # 配置文件读取函数
     def load_cofig(self):
-        with open(self.config_file, "r") as f:
-            config = json.load(f)
+        # 第一种方式，从main.json读取案例配置文件路径
+        if self.get_param_from == "FROM_MAIN_JSON":
+            self.json_file = Path(__file__).parent.parent / "config/main.json"
+            with open(self.json_file, "r") as f1:
+                main_config = json.load(f1)
+                self.case_file = Path(main_config["case_file"]).resolve()
+        elif self.get_param_from == "FROM_CASE_JSON":
+            # 第二种方式，如果直接给定案例配置，则直接读取
+            self.case_file = self.json_file
+
+        with open(self.case_file, "r") as f2:
+            config = json.load(f2)
+
+        # 必要字段校验
+        required_fields = ["debug_level", "input_file", "output_file", "parts"]
+        for field in required_fields:
+            if field not in config:
+                raise ValueError(f"配置文件中缺少必要字段: {field}")
 
         self.debug_level = config["debug_level"]
         self.input_file = config["input_file"]
         self.output_file = config["output_file"]
-
-        if "parts" not in config:
-            raise ValueError("配置文件中缺少 parts 字段")
 
         self.part_params = []
         for part in config["parts"]:
