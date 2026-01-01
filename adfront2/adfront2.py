1e4e3fe7ccff6a1b2abf3576dab62d6c61634644
cfd_dev <cfd_dev@126.com>
2026-01-01 10:37:07 +0800

    fix: 重新启用基准阵面搜索半径异常检查
    当基准阵面搜索半径超过20时抛出异常，防止潜在问题发生

--- adfront2/adfront2.py
+++ adfront2/adfront2.py
@@ -389,9 +389,9 @@ class Adfront2:
             heapq.heappush(self.front_list, self.base_front)  # 重新将基准阵面加入堆中
             # self.debug_level = 1
 
-        # if self.base_front.al > 20:
-        #     # 异常退出
-        #     raise Exception("基准阵面搜索半径超过20，可能存在问题")
+        if self.base_front.al > 20:
+            # 异常退出
+            raise Exception("基准阵面搜索半径超过20，可能存在问题")
 
         return self.pselected
 
