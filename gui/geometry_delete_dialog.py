c72f6a6cc3cf1ea14eef15071a1110fe9809d005
cfd_dev <cfd_dev@126.com>
2026-01-24 09:09:34 +0800

    feat(几何删除): 添加中键确认功能以增强几何元素删除体验

--- gui/geometry_delete_dialog.py
+++ gui/geometry_delete_dialog.py
@@ -33,6 +33,7 @@ class GeometryDeleteDialog(QDialog):
 
         self.geometry_data = getattr(self.gui, "current_geometry", None)
         self._picking_helper = None
+        self._middle_observer_id = None
         self.selected_geometry_elements = {
             "vertices": set(),
             "edges": set(),
@@ -215,12 +216,38 @@ class GeometryDeleteDialog(QDialog):
             on_unpick=self._on_geometry_unpick,
         )
         self._picking_helper.enable()
+        self._bind_middle_confirm()
 
     def _disable_picking(self):
         if self._picking_helper is None:
             return
+        self._unbind_middle_confirm()
         self._picking_helper.cleanup()
         self._picking_helper = None
+
+    def _bind_middle_confirm(self):
+        interactor = None
+        if self.gui and hasattr(self.gui, "mesh_display") and getattr(self.gui.mesh_display, "frame", None) is not None:
+            interactor = self.gui.mesh_display.frame.GetRenderWindow().GetInteractor()
+        if interactor is None:
+            return
+        if self._middle_observer_id is None:
+            self._middle_observer_id = interactor.AddObserver("MiddleButtonPressEvent", self._on_middle_button_press)
+
+    def _unbind_middle_confirm(self):
+        if self._middle_observer_id is None:
+            return
+        interactor = None
+        if self.gui and hasattr(self.gui, "mesh_display") and getattr(self.gui.mesh_display, "frame", None) is not None:
+            interactor = self.gui.mesh_display.frame.GetRenderWindow().GetInteractor()
+        if interactor is not None:
+            interactor.RemoveObserver(self._middle_observer_id)
+        self._middle_observer_id = None
+
+    def _on_middle_button_press(self, obj, event):
+        if not self.isVisible():
+            return
+        self.accept()
 
     def _on_geometry_pick(self, element_type, element_obj, element_index):
         key_map = {
